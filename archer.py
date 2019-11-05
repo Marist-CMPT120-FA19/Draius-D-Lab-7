7f3ce42dbec48bc5ba358882aa272c77ed1778c1
Draius <41041066+draius@users.noreply.github.com>
2019-11-05 15:49:28 -0500

    Complete Fix

--- archer.py
+++ archer.py
@@ -1,54 +1,64 @@
 #Draius Disimone
 #CMPT120
-
 from graphics import *
 import math
 
-def location(pt1, circle):
-    dx = pt1.getX() - circle.getCenter().getX()
-    dy = pt1.getY() - circle.getCenter().getY()
-    
-    dist = math.sqrt(dx * dx + dy * dy)
+def distform(point, circle):
+    x = point.getX() - circle.getCenter().getX()
+    y = point.getY() - circle.getCenter().getY()
+    dist = math.sqrt(x*x + y*y)
 
     return dist <= circle.getRadius()
+
     
 def main():
-    win = GraphWin("Archer Table" , 1000 , 1000)
-    yellow = Circle(Point(500 , 500) , 75)
-    white = Circle(Point(500 , 500) , 375)
-    black = Circle(Point(500 , 500) , 300)
-    red = Circle(Point(500 , 500) , 150)
-    blue = Circle(Point(500 , 500) , 225)
+
+    score=0
     
-    yellow.setFill("yellow")
+    win = GraphWin('Archery',300,300)
+    center=Point(150, 150)
+
+    white=Circle(center,100)
     white.setFill("white")
-    black.setFill("black")
-    red.setFill("red")
-    blue.setFill("blue")
-   
-    yellow.draw(win)
     white.draw(win)
+
+    black=Circle(center,80)
+    black.setFill("black")
     black.draw(win)
-    red.draw(win)
+
+    blue=Circle(center, 60)
+    blue.setFill("blue")
     blue.draw(win)
-    
-    score = 0
-    total = 0
 
-    for i in range(5):
-        mouse = win.getMouse()
-        if location(mouse , yellow):
-            score = 90
-        elif locationn(mouse , red):
-            score = 70
-        elif location(mouse , blue):
-            score = 50
-        elif locationn(mouse , black):
-            score = 30
-        elif location(mouse , white):
-            score = 10
-        print("Your click was worth " , score , " points")
-        total += score
-    print("Total Score = " , total)
+    red=Circle(center, 40)
+    red.setFill("red")
+    red.draw(win)
 
+    yellow=Circle(center,20)
+    yellow.setFill("yellow")
+    yellow.draw(win)
+
+    for x in range(5):
+        click= win.getMouse()
+        if(distform(click, yellow)):
+            score += 9
+            print("+9")
+        elif(distform(click, red)):
+            score += 7
+            print("+7")
+        elif(distform(click, blue)):
+            score += 5
+            print("+5")
+        elif(distform(click, black)):
+            score += 3
+            print("+3")
+        elif(distform(click, white)):
+            score += 1
+            print("+1")
+        else:
+            score += 0
+            print("+0")  
+    print("Total Points: ", score)                 
+    win.getMouse()
+    win.close()
 main()
